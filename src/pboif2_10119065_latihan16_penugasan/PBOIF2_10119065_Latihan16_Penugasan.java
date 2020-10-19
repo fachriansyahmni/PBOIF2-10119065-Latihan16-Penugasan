e8ac2f08cd0ebdf9139f5abc9b27082951a33867
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-19 14:50:44 +0700

    upload

---
+++ src/pboif2_10119065_latihan16_penugasan/PBOIF2_10119065_Latihan16_Penugasan.java
@@ -0,0 +1,52 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini berisi Penugasan
+ *
+ */
+package pboif2_10119065_latihan16_penugasan;
+
+public class PBOIF2_10119065_Latihan16_Penugasan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c +"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= a;
+        System.out.println("c >>= a = " + c);
+        c &= 2;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+}
